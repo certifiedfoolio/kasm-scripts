db695a9c6d3be392445771fe054d116c259394b7
uhidontkno <hi@rare1k.dev>
2024-03-22 16:33:38 -0400

    feat: use apt installer

--- discord.sh
+++ discord.sh
@@ -1,6 +1,10 @@
-# This installs the AppImage version to be universally compatible
-curl https://github.com/ArmCord/ArmCord/releases/download/v3.2.6/ArmCord-3.2.6.AppImage > armcord.AppImage
-chmod +x armcord.AppImage
-#mv ArmCord-3.2.6.AppImage armcord.AppImage
-echo "ArmCord has been downloaded in your current working directory."
-echo "Run ./armcord.AppImage to run ArmCord (Discord)"
+echo "[ ########## WARNING! PLEASE READ!!! ########## ]"
+echo "THIS ONLY WORKS ON DEBIAN/UBUNTU CONTAINERS -----"
+echo "IF YOU ARENT IN ONE SWITCH TO ONE OR PRESS CTRL+C"
+echo "Waiting 3 seconds..."
+sleep 3
+
+curl -fsSL https://apt.armcord.app/public.gpg | sudo gpg --dearmor -o /usr/share/keyrings/armcord.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/armcord.gpg] https://apt.armcord.app/ stable main" | sudo tee /etc/apt/sources.list.d/armcord.list
+sudo apt update
+sudo apt install armcord
