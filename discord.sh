be20f0a20803a515af3fd876abd33be18fda57be
uhidontkno <hi@rare1k.dev>
2024-03-22 16:11:26 -0400

    feat: add armcord installer

---
+++ discord.sh
@@ -0,0 +1,5 @@
+# This installs the AppImage version to be universally compatible
+curl -O armcord.AppImage https://github.com/ArmCord/ArmCord/releases/download/v3.2.6/ArmCord-3.2.6.AppImage
+chmod +x armcord.AppImage
+echo "ArmCord has been downloaded in your current working directory."
+echo "Run ./armcord.AppImage to run ArmCord (Discord)"
