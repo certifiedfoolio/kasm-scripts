378935474f8d9577fb93fe8038f25a37e54ad31b
uhidontkno <hi@rare1k.dev>
2024-03-22 16:29:28 -0400

    feat: use curl again but redirect differently

--- discord.sh
+++ discord.sh
@@ -1,6 +1,6 @@
 # This installs the AppImage version to be universally compatible
-wget https://github.com/ArmCord/ArmCord/releases/download/v3.2.6/ArmCord-3.2.6.AppImage > /dev/null 2> /dev/null 
-chmod +x ArmCord-3.2.6.AppImage
-mv ArmCord-3.2.6.AppImage armcord.AppImage
+curl https://github.com/ArmCord/ArmCord/releases/download/v3.2.6/ArmCord-3.2.6.AppImage > armcord.AppImage
+chmod +x armcord.AppImage
+#mv ArmCord-3.2.6.AppImage armcord.AppImage
 echo "ArmCord has been downloaded in your current working directory."
 echo "Run ./armcord.AppImage to run ArmCord (Discord)"
