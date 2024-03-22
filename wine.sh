f268012a44ceca6925fff4cde5f500b0e78bc269
uhidontkno <hi@rare1k.dev>
2024-03-22 16:57:35 -0400

    feat: add wine for lazy asses

---
+++ wine.sh
@@ -0,0 +1,12 @@
+echo "[ ########## WARNING! PLEASE READ!!! ########## ]"
+echo "THIS ONLY WORKS ON DEBIAN/UBUNTU CONTAINERS -----"
+echo "IF YOU ARENT IN ONE SWITCH TO ONE OR PRESS CTRL+C"
+echo "Waiting 3 seconds..."
+sleep 3
+
+DEBIAN_FRONTEND=noninteractive
+sudo apt -y install wine
+
+echo "wine is installed. you could of done:"
+echo "sudo apt -y install wine"
+echo "you lazy ass"
