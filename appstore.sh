59ea5fce05686f5fea9436fde261e2f3f12e006d
uhidontkno <hi@rare1k.dev>
2024-03-22 16:23:46 -0400

    feat: app store installer

---
+++ appstore.sh
@@ -0,0 +1,25 @@
+if [ "$EUID" -ne 0 ]; then
+    printf "$BBLUE[i]$OFF |$BLUE This script needs to be run as root. Attempting to elevate...$OFF\n"
+    sudo "bash" "$0" "$@"  
+    exit $?
+fi
+
+
+echo "[ ########## WARNING! PLEASE READ!!! ########## ]"
+echo "THIS ONLY WORKS ON DEBIAN/UBUNTU CONTAINERS -----"
+echo "IF YOU ARENT IN ONE SWITCH TO ONE OR PRESS CTRL+C"
+echo "Waiting 3 seconds..."
+sleep 3
+
+# Install SNAP
+sudo apt update
+sudo apt install snapd
+sudo snap install core
+sudo snap install snap-store
+
+# Install Flatpak
+sudo apt install flatpak
+sudo apt install gnome-software-plugin-flatpak
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+echo "Snap and Flatpak have been installed successfully."
